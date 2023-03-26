e18c998adbea69cd2291ab692dbf03c7bc4325e6
Trofim <trofimtyulkin@gmail.com>
2023-03-26 16:53:47 +0500

    рефакторинг кода

--- bot/src/main/java/ru/tinkoff/edu/HelloController.java
+++ bot/src/main/java/ru/tinkoff/edu/HelloController.java
@@ -1,9 +1,7 @@
 package ru.tinkoff.edu;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
