ada5af84ada02bad480c126ffc6d0f915482546e
Trofim <trofimtyulkin@gmail.com>
2023-04-05 00:13:06 +0500

    выполнена домашка 4

--- bot/src/main/java/ru/tinkoff/edu/config/ApplicationConfig.java
+++ bot/src/main/java/ru/tinkoff/edu/config/ApplicationConfig.java
@@ -2,8 +2,15 @@ package ru.tinkoff.edu.config;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
+import ru.tinkoff.edu.Bot;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = true)
-public record ApplicationConfig(@NotNull String test) {}
+public record ApplicationConfig(@NotNull String test) {
+    @Bean
+    public String test(ApplicationConfig config) {
+        return config.test;
+    }
+}
--- scrapper/src/main/java/ru/tinkoff/edu/ScrapperApplication.java
+++ scrapper/src/main/java/ru/tinkoff/edu/ScrapperApplication.java
@@ -15,6 +15,5 @@ public static void main(String[] args) {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
         //ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
         //System.out.println(config);
-        System.out.println(ctx.getBean(StackOverflowClient.class).fetchQuestion(42365266));
         }
         }
